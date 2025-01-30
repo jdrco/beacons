d2b0bf74785ddbf7d737f28a9786c87b56cc3bd8
Jared Drueco <drueco03@gmail.com>
2025-01-30 17:14:26 +0000

    feat: use custom mapbox studio style

--- web/components/Map.tsx
+++ web/components/Map.tsx
@@ -19,57 +19,7 @@ const Map = ({ className = '' }: MapProps) => {
 
     mapRef.current = new mapboxgl.Map({
       container: mapContainerRef.current,
-      style: 'mapbox://styles/mapbox/standard',
-      center: [-113.53, 53.52],
-      zoom: 15.5,
-      pitch: 45,
-      bearing: -17.6,
-      antialias: true
-    })
-
-    mapRef.current.on('style.load', () => {
-      if (!mapRef.current) return
-
-      const layers = mapRef.current.getStyle().layers
-      const labelLayerId = layers.find(
-        (layer) => layer.type === 'symbol' && layer.layout?.['text-field']
-      )?.id
-
-      if (!labelLayerId) return
-
-      mapRef.current.addLayer(
-        {
-          id: 'add-3d-buildings',
-          source: 'composite',
-          'source-layer': 'building',
-          filter: ['==', 'extrude', 'true'],
-          type: 'fill-extrusion',
-          minzoom: 15,
-          paint: {
-            'fill-extrusion-color': '#aaa',
-            'fill-extrusion-height': [
-              'interpolate',
-              ['linear'],
-              ['zoom'],
-              15,
-              0,
-              15.05,
-              ['get', 'height']
-            ],
-            'fill-extrusion-base': [
-              'interpolate',
-              ['linear'],
-              ['zoom'],
-              15,
-              0,
-              15.05,
-              ['get', 'min_height']
-            ],
-            'fill-extrusion-opacity': 0.6
-          }
-        },
-        labelLayerId
-      )
+      style: 'mapbox://styles/jdrco/cm649khvj002601sthc0bc7la',
     })
 
     return () => {

011db71650c6f970bed4af8aaf100bccb799a32f
Jared Drueco <drueco03@gmail.com>
2025-04-08 06:03:21 +0000

    fix: more build errs

--- web/components/SignUpForm.tsx
+++ web/components/SignUpForm.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import { useRouter } from "next/navigation";
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
